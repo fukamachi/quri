3aedf3374e0e44a2db2dedaa7035b63f03feb077
Eitaro Fukamachi <e.arrows@gmail.com>
2014-10-26 00:38:00 +0900

    Parse userinfo in authority.

--- src/parser.lisp
+++ src/parser.lisp
@@ -10,7 +10,7 @@
 
 (defun parse-uri (string)
   (check-type string string)
-  (let (scheme authority path query fragment
+  (let (scheme userinfo host path query fragment
         (len (length string)))
     (block nil
       (flet ((parse-from-path (string start)
@@ -31,11 +31,14 @@
                 ;; assume this is a relative uri.
                 (return (parse-from-path string 0))))
           (setq scheme (subseq string start end))
-          (multiple-value-bind (string start end)
+          (multiple-value-bind (string user-start user-end pass-start pass-end
+                                host-start host-end port-start port-end)
               (parse-authority string :start (1+ end) :end len)
-            (setq authority (subseq string start end))
-            (parse-from-path string end)))))
-    (values scheme authority path query fragment)))
+            (when user-start
+              (setq userinfo (subseq string user-start (or pass-end user-end))))
+            (setq host (subseq string host-start (or port-end host-end)))
+            (parse-from-path string (or port-end host-end))))))
+    (values scheme userinfo host path query fragment)))
 
 (defun parse-scheme (string &key (start 0) (end (length string)))
   (declare (type string string)
@@ -68,7 +71,16 @@
   (declare (type string string)
            (optimize (speed 3) (safety 2))
            #+sbcl (sb-ext:muffle-conditions sb-ext:compiler-note))
-  (let ((authority-mark nil))
+  (let ((authority-mark nil)
+        (colon-mark nil)
+        username-start
+        username-end
+        password-start
+        password-end
+        host-start
+        host-end
+        port-start
+        port-end)
     (with-array-parsing (char p string start end)
       (declare (type character char))
       (parsing-first
@@ -76,8 +88,7 @@
          ((char= char #\/)
           (gonext))
          (T
-          (return-from parse-authority
-            (values string start p)))))
+          (error 'uri-malformed-string))))
 
       (parsing-authority-starting
        (unless (char= char #\/)
@@ -88,18 +99,45 @@
        (setq authority-mark p)
        (gonext 0))
 
+      ;; parsing host or userinfo
       (parsing-authority
        (cond
+         ((char= char #\:)
+          (setq colon-mark p)
+          (redo))
+         ((char= char #\@)
+          (when username-start
+            (error 'uri-malformed-string))
+          (if colon-mark
+              (setq username-start authority-mark
+                    username-end colon-mark
+                    password-start (1+ colon-mark)
+                    password-end p)
+              (setq username-start authority-mark
+                    username-end p))
+          (setq authority-mark (1+ p)
+                colon-mark nil)
+          (redo))
          ((or (char= char #\/)
               (char= char #\?)
               (char= char #\#))
-          (return-from parse-authority
-            (values string authority-mark p)))
+          (go :eof))
          (T (redo))))
 
       (:eof
+       (if colon-mark
+           (setq host-start authority-mark
+                 host-end colon-mark
+                 port-start (1+ colon-mark)
+                 port-end p)
+           (setq host-start authority-mark
+                 host-end p))
        (return-from parse-authority
-         (values string (or authority-mark p) end))))))
+         (values string
+                 username-start username-end
+                 password-start password-end
+                 host-start host-end
+                 port-start port-end))))))
 
 (defmacro parse-until (delimiters string &key (start 0) (end (length string)))
   (with-gensyms (p char)
