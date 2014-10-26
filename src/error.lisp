a87145f0be32ecf1c9bf3b3f0c18309b36b3fa61
Eitaro Fukamachi <e.arrows@gmail.com>
2014-10-27 05:26:37 +0900

    Delete unnecessary error classes.

--- src/error.lisp
+++ src/error.lisp
@@ -3,30 +3,17 @@
   (:use :cl)
   (:export :uri-error
 
-           :uri-parse-error
-
            :uri-malformed-string
            :uri-invalid-scheme
            :uri-invalid-port
 
-           :uri-callback-error))
+           :uri-malformed-urlencoded-string))
 (in-package :quri.error)
 
 (define-condition uri-error (simple-error) ())
 
-(define-condition uri-parse-error (uri-error) ())
-
-(define-condition uri-malformed-string (uri-parse-error) ())
+(define-condition uri-malformed-string (uri-error) ())
 (define-condition uri-invalid-scheme (uri-malformed-string) ())
 (define-condition uri-invalid-port (uri-malformed-string) ())
-(define-condition uri-malformed-urlencoded-string (uri-malformed-string) ())
 
-(define-condition uri-callback-error (uri-error)
-  ((name :initarg :name
-         :initform (error ":name is required"))
-   (error :initarg :error
-          :initform (error ":error is required")))
-  (:report (lambda (condition stream)
-             (format stream "Error while executing a callback: ~S~%    ~A"
-                     (slot-value condition 'name)
-                     (slot-value condition 'error)))))
+(define-condition uri-malformed-urlencoded-string (uri-error) ())
