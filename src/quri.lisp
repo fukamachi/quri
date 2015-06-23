69d5ba6ed097b137b4dc97a579a3a0bb792fd100
Thayne McCombs <astrothayne@gmail.com>
2015-06-22 22:57:37 -0600

    Add merg-uris function.

--- src/quri.lisp
+++ src/quri.lisp
@@ -28,6 +28,7 @@
   (:import-from :quri.encode
                 :url-encode
                 :url-encode-params)
+  (:import-from :split-sequence :split-sequence)
   (:export :parse-uri
            :parse-scheme
            :parse-authority
@@ -82,6 +83,7 @@
 
            :copy-uri
            :render-uri
+           :merge-uris
 
            :url-decode
            :url-decode-params
@@ -181,3 +183,67 @@
   (format stream "#<~S ~A>"
           (type-of uri)
           (render-uri uri)))
+
+
+(defun merge-uri-paths (ref-path base-path)
+  (declare (type (or string null) ref-path base-path))
+  (let* ((path-list (and base-path (nreverse (split-sequence #\/ base-path))))
+         (ref-components (and ref-path (split-sequence #\/ ref-path)))
+         ending-slash-p)
+    ;; remove last component of base
+    (pop path-list)
+    (dolist (component ref-components)
+      (cond ((string= ".." component)
+             (pop path-list)
+             (setf ending-slash-p t))
+            ((string= "." component)
+             (setf ending-slash-p t))
+            (t
+             (push component path-list)
+             (setf ending-slash-p nil))))
+    (setf path-list (nreverse path-list))
+    (with-output-to-string (s)
+      (loop for (component . more) on path-list
+         do (progn
+              (write-string component s)
+              (when (or more ending-slash-p)
+                (write-char #\/ s)))))))
+
+(defun merge-uris (reference base)
+  (declare (uri reference base))
+  "Merge a reference URI into the base URI as described in RFC 2396 Section 5.2.
+The returned URI may or may not be a new instance. Neither REFERENCE nor BASE is
+mutated."
+  ;; Step 2 -- return base if same document
+  (when (and (null (uri-path reference))
+             (null (uri-scheme reference))
+             (null (uri-authority reference))
+             (null (uri-query reference)))
+    (return-from merge-uris base))
+
+  ;; Step 3 -- scheme
+  (when (uri-scheme reference)
+    (return-from merge-uris reference))
+  (let ((uri (copy-uri reference :scheme (uri-scheme base))))
+    (when (null (uri-port uri))
+      (setf (uri-port uri) (quri.port:scheme-default-port (uri-scheme uri))))
+    (macrolet ((done () '(return-from merge-uris uri)))
+
+      ;; Step 4 -- Authority
+      (when (uri-host uri)
+        (done))
+      (setf (uri-userinfo uri) (uri-userinfo base))
+      (setf (uri-host uri) (uri-host base))
+      (setf (uri-port uri) (uri-port base))
+
+      ;; Step 5 -- Absolute path
+      (alexandria:when-let* ((p (uri-path uri))
+                             (first-char (and (> (length p) 0) (char p 0))))
+        (when (char= #\/ first-char)
+          (done)))
+
+      ;; Step 6 -- Relative path
+      (setf (uri-path uri) (merge-uri-paths (uri-path uri) (uri-path base)))
+
+      ;; Step 7 -- Finish
+      (done))))
--- t/quri.lisp
+++ t/quri.lisp
@@ -2,7 +2,8 @@
 (defpackage quri-test
   (:use :cl
         :quri
-        :prove))
+        :prove)
+  (:import-from :quri.uri :make-uri))
 (in-package :quri-test)
 
 (plan nil)
@@ -61,5 +62,22 @@
   (subtest (format nil "~A (copy-uri)" test-uri)
     (let ((uri (uri test-uri)))
       (is uri (copy-uri uri) :test #'uri=))))
+
+(defparameter *base-uri* (uri "http://www.example.com/path/a/b.html"))
+
+(defparameter *merge-test-cases*
+  `((,(uri "file:///tmp/junk.txt") . "file:///tmp/junk.txt")
+    (,(make-uri :userinfo "auth" :host "secretplace.com") . "http://auth@secretplace.com")
+    (,(make-uri :host "example.com" :path "/path" :query "query") . "http://example.com/path?query")
+    (,(uri "/new/path") . "http://www.example.com/new/path")
+    (,(uri "foo.txt") . "http://www.example.com/path/a/foo.txt")
+    (,(uri "../bar") . "http://www.example.com/path/bar")
+    (,(uri "other/./car") . "http://www.example.com/path/a/other/car")
+    (,(uri "./../.") . "http://www.example.com/path/")))
+
+(subtest "merge-uris"
+  (loop for (test-uri . result-uri) in *merge-test-cases* do
+       (let ((merged-uri (merge-uris test-uri *base-uri*)))
+         (is (render-uri merged-uri) result-uri :test 'string=))))
 
 (finalize)
