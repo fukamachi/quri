91298a5d4a4e5c4500f7f5deb0ab867ab9f1c75f
Eitaro Fukamachi <e.arrows@gmail.com>
2015-08-05 13:50:50 +0900

    Add a test.

--- t/encode.lisp
+++ t/encode.lisp
@@ -11,7 +11,9 @@
   (is (url-encode "Tiffany") "Tiffany")
   (is (url-encode "Tiffany & Co.") "Tiffany%20%26%20Co.")
   (is (url-encode "Tiffany & Co." :space-to-plus t)
-      "Tiffany+%26+Co."))
+      "Tiffany+%26+Co.")
+  (is (url-encode "{\"field\": \"test\", \"data\": 0, \"products\": {\"name\": \"apples\"}, \"status\": true}")
+      "%7B%22field%22%3A%20%22test%22%2C%20%22data%22%3A%200%2C%20%22products%22%3A%20%7B%22name%22%3A%20%22apples%22%7D%2C%20%22status%22%3A%20true%7D"))
 
 (subtest "url-encode-params"
   (is (url-encode-params '(("a" . "b") ("c" . "d")))
